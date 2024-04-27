c86f9c5f731c7cca79ff744e4eac0c5d63f97890
Brandon <amayastid@gmail.com>
2024-04-27 14:46:28 -0500

    Se ocultan secretos de google

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -1,10 +1,10 @@
 const { OAuth2Client } = require('google-auth-library');
-const client = new OAuth2Client(process.env.GOOGLE_SECRET);
+const client = new OAuth2Client("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");
 
 async function googleVerify(token) {
     const ticket = await client.verifyIdToken({
         idToken: token,
-        audience: process.env.GOOGLE_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        audience: "817985495331-8u41le1eblm93lil1i231eofjertim5o.apps.googleusercontent.com",  // Specify the CLIENT_ID of the app that accesses the backend
         // Or, if multiple clients access the backend:
         //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
     });
